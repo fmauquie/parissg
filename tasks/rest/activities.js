c083f6540cec12ad47d57672cb7ca6ae1ced6547
Thierry ABALEA <t.abalea@gmail.com>
2014-10-24 16:21:12 +0200

    Add movies activies from Allocine data API

--- tasks/rest/activities.js
+++ tasks/rest/activities.js
@@ -7,6 +7,131 @@ restSupport.get({
     next(null,
         [
                 {
+                    "idactivites": 9005,
+                    "source": "allocine",
+                    "name": "Film \"Les âmes noires\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/pictures/14/09/16/12/53/103140.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-24T00:00:00.000Z",
+                            "hour_start": "20:00:00",
+                            "hour_end": "20:00:00"
+                        },
+                        {
+                            "jour": "2014-10-25T00:00:00.000Z",
+                            "hour_start": "22:00:00",
+                            "hour_end": "22:00:00"
+                        },
+                        {
+                            "jour": "2014-10-26T00:00:00.000Z",
+                            "hour_start": "20:00:00",
+                            "hour_end": "20:00:00"
+                        },
+                        {
+                            "jour": "2014-10-27T00:00:00.000Z",
+                            "hour_start": "12:15:00",
+                            "hour_end": "12:15:00"
+                        }
+                    ]
+                },
+                {
+                    "idactivites": 9006,
+                    "source": "allocine",
+                    "name": "Film \"Minopolska\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/pictures/14/06/02/10/53/584482.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-28T00:00:00.000Z",
+                            "hour_start": "11:00:00",
+                            "hour_end": "11:00:00"
+                        }
+                    ]
+                },
+                {
+                    "idactivites": 9007,
+                    "source": "allocine",
+                    "name": "Film \"Métamorphoses\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/pictures/14/09/05/15/52/597917.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-24T00:00:00.000Z",
+                            "hour_start": "22:00:00",
+                            "hour_end": "22:00:00"
+                        },
+                        {
+                            "jour": "2014-10-26T00:00:00.000Z",
+                            "hour_start": "22:00:00",
+                            "hour_end": "22:00:00"
+                        },
+                        {
+                            "jour": "2014-10-27T00:00:00.000Z",
+                            "hour_start": "20:00:00",
+                            "hour_end": "20:00:00"
+                        },
+                        {
+                            "jour": "2014-10-28T00:00:00.000Z",
+                            "hour_start": "22:00:00",
+                            "hour_end": "22:00:00"
+                        },
+                        {
+                            "jour": "2014-10-25T00:00:00.000Z",
+                            "hour_start": "20:00:00",
+                            "hour_end": "20:00:00"
+                        }
+                    ]
+                },
+                {
                     "idactivites": 88953,
                     "source": "paris api",
                     "name": "Sc&egrave;nes d'&eacute;t&eacute; &agrave; la Villette",
@@ -684,6 +809,221 @@ restSupport.get({
                     "categories": ["Sport"],
                     "files": [],
                     "occurences": []
+                },
+                {
+                    "idactivites": 9008,
+                    "source": "allocine",
+                    "name": "Film \"Hautes-Terres\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/pictures/14/06/26/10/48/587761.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-28T00:00:00.000Z",
+                            "hour_start": "20:00:00",
+                            "hour_end": "20:00:00"
+                        }
+                    ]
+                },
+                {
+                    "idactivites": 9009,
+                    "source": "allocine",
+                    "name": "Film \"Le Bal des vampires\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/35/59/99/19355043.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-26T00:00:00.000Z",
+                            "hour_start": "18:00:00",
+                            "hour_end": "18:00:00"
+                        }
+                    ]
+                },
+                {
+                    "idactivites": 9010,
+                    "source": "allocine",
+                    "name": "Film \"Qui veut la peau de Roger Rabbit ?\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/66/84/62/18959140.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-24T00:00:00.000Z",
+                            "hour_start": "16:00:00",
+                            "hour_end": "16:00:00"
+                        },
+                        {
+                            "jour": "2014-10-26T00:00:00.000Z",
+                            "hour_start": "14:00:00",
+                            "hour_end": "14:00:00"
+                        }
+                    ]
+                },
+                {
+                    "idactivites": 9011,
+                    "source": "allocine",
+                    "name": "Film \"Une vie de chat\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/63/78/79/19510694.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-28T00:00:00.000Z",
+                            "hour_start": "10:15:00",
+                            "hour_end": "10:15:00"
+                        }
+                    ]
+                },
+                {
+                    "idactivites": 9012,
+                    "source": "allocine",
+                    "name": "Film \"E.T. l'extra-terrestre\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/00/02/36/52/affet.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-25T00:00:00.000Z",
+                            "hour_start": "14:00:00",
+                            "hour_end": "14:00:00"
+                        }
+                    ]
+                },
+                {
+                    "idactivites": 9013,
+                    "source": "allocine",
+                    "name": "Film \"Freaks\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/00/02/24/09/affiche.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-25T00:00:00.000Z",
+                            "hour_start": "18:15:00",
+                            "hour_end": "18:15:00"
+                        }
+                    ]
+                },
+                {
+                    "idactivites": 9014,
+                    "source": "allocine",
+                    "name": "Film \"J'arrête quand je veux\"",
+                    "description": "",
+                    "small_description": "",
+                    "lieu": "Le Nouveau Latina",
+                    "address": "20, rue du Temple - Paris 4e arrondissement",
+                    "geolocation": "48.858615,2.353515",
+                    "distance": 0.25,
+                    "hasFee": "0",
+                    "categories": [
+                        "Film"
+                    ],
+                    "files": [
+                        {
+                            "path": "http://images.allocine.fr/pictures/14/05/21/16/08/372748.jpg"
+                        },
+                        {
+                            "path": "http://images.allocine.fr/medias/nmedia/18/70/85/59/19698628.jpg"
+                        }
+                    ],
+                    "occurences": [
+                        {
+                            "jour": "2014-10-27T00:00:00.000Z",
+                            "hour_start": "22:00:00",
+                            "hour_end": "22:00:00"
+                        }
+                    ]
                 }
             ]
     );
